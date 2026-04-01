b1403a00867fe058b445c8f9f6661c3d8bdd6369
ovenstore <ostory674@gmail.com>
2026-04-01 09:58:36 +0200

    Add User model and auth packages

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const Score = require('./models/Score')
+const User = require('./models/User')
+
 const mongoose = require('mongoose')
 
 const questions = require('./data/questions')
